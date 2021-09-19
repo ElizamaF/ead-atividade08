b852e38ce5c28e01f70b3b495ecff2ad6e10ccbb
Elizama Fernandes dos Santos <elizamafernandesmgn@gmail.com>
2021-09-19 09:23:27 +0200

    Segunda versao do programa ICM com estrutura 'if-else'

--- IMC.php
+++ IMC.php
@@ -1,13 +1,26 @@
 <?php
 
-    $peso = 60;
-    $altura = 1.74;
-    $IMC;
+    $peso = 60; //peso em kg
+    $altura = 1.74; //altura em metros
+    $IMC; //IMC = índice massa corpórea
 
     $IMC = $peso / ($altura * $altura);
 
     echo "Agora será calculado o seu IMC conforme os seus dados.<br><br>";
-    echo "O seu IMC é: $IMC";
+    echo "O seu IMC é: $IMC<br><br>";
+
+    if($IMC < 20){
+        echo "O seu peso é considerado baixo.";
+    }
+
+    else if($IMC > 25){
+        echo "Voce tem sobrepeso. Recomendamos praticar mais exercício físico.";
+    }
+
+    else {
+        echo "O seu peso é ideal. Parabéns!";
+    }
+
 
 
 ?>
