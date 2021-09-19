626a452b89e2f0e27045bd77275597da66a9340c
Elizama Fernandes dos Santos <elizamafernandesmgn@gmail.com>
2021-09-19 09:11:02 +0200

    Primeira versao do programa IMC

---
+++ IMC.php
@@ -0,0 +1,13 @@
+<?php
+
+    $peso = 60;
+    $altura = 1.74;
+    $IMC;
+
+    $IMC = $peso / ($altura * $altura);
+
+    echo "Agora será calculado o seu IMC conforme os seus dados.<br><br>";
+    echo "O seu IMC é: $IMC";
+
+
+?>
